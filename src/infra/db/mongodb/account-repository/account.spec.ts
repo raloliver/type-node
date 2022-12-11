cbfe0bf0728dd95a022977a7c01af37423aa464c
raloliver <contato@raloliver.com.br>
2022-12-11 08:05:02 +0000

    refactor: clear db between test

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -2,12 +2,16 @@
  * File: account.spec.ts
  * Project: type-node
  * Created: Sunday, December 11th 2022, 6:50:54 am
- * Last Modified: Sunday, December 11th 2022, 7:28:07 am
+ * Last Modified: Sunday, December 11th 2022, 8:04:22 am
  * Copyright © 2022 AMDE Agência
  */
 
 import {MongoDBHelper} from '../helpers/mongodb.helper'
 import {AccountMongoDBRepository} from './account'
+
+const accountFactory = (): AccountMongoDBRepository => {
+  return new AccountMongoDBRepository()
+}
 
 describe('Account MongoDB Repository', () => {
   //connect on DB before and desconect after
@@ -19,8 +23,14 @@ describe('Account MongoDB Repository', () => {
     await MongoDBHelper.close()
   })
 
+  // clear db between tests
+  beforeEach(async () => {
+    const accountCollection = MongoDBHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should create an account success', async () => {
-    const sut = new AccountMongoDBRepository()
+    const sut = accountFactory()
     const account = await sut.add({
       name: 'João da Silva (Integration Test)',
       email: 'joao_test@joaodasilva.com.br',
